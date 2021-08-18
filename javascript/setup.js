ba3c01f05eb87e43f6903536c7cd745d9e24d0ee
Mark Vasilkov <mvasilkov@gmail.com>
2021-08-18 18:34:30 +0300

    Basic layout

--- javascript/setup.js
+++ javascript/setup.js
@@ -4,13 +4,23 @@ const TOTAL_CANNONS = 12
 const CANNON_BUILD_TIME = 50 // ticks, 1 second = 50 ticks
 const CANNON_RELOAD_TIME = 100
 
+const GAME_CANVAS_HEIGHT = 540
+const GAME_CANVAS_WIDTH = 540
+
+const FONTS = `-apple-system, 'Segoe UI', 'DejaVu Sans', system-ui, sans-serif`
+
 const canvases = {
+    a: document.getElementById('a'),
     b: document.getElementById('b'),
 }
 
 const cons = {
+    a: canvases.a.getContext('2d'),
     b: canvases.b.getContext('2d'),
 }
+
+canvases.a.height = GAME_CANVAS_HEIGHT
+canvases.a.width = GAME_CANVAS_WIDTH
 
 canvases.b.height = BRAILLE_CANVAS_HEIGHT
 canvases.b.width = BRAILLE_CANVAS_WIDTH
