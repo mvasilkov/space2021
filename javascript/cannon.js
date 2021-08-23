3bcde524c208b0f5986014b978b3c281a4cbf7db
Mark Vasilkov <mvasilkov@gmail.com>
2021-08-23 22:57:05 +0300

    Invaders have coordinates

--- javascript/cannon.js
+++ javascript/cannon.js
@@ -2,8 +2,10 @@
 
 const CANNON_MISSING = 0
 const CANNON_BUILDING = 1
-const CANNON_READY = 2
-const CANNON_RELOADING = 3
+const CANNON_UPGRADING = 2
+const CANNON_READY = 3
+const CANNON_RELOADING = 4
+const CANNON_RECYCLING = 5
 
 class Cannon {
     constructor() {
