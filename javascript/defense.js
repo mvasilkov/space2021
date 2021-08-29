bb308002337bc7dd4da55a537798c78313236eb3
Mark Vasilkov <mvasilkov@gmail.com>
2021-08-29 22:31:07 +0300

    Cannons prep

--- javascript/defense.js
+++ javascript/defense.js
@@ -4,16 +4,18 @@ const PLATFORM_MISSING = 0
 const PLATFORM_BUILDING = 1
 const PLATFORM_UPGRADING = 2
 const PLATFORM_READY = 3
-const PLATFORM_RELOADING = 4
-const PLATFORM_RECYCLING = 5
+const PLATFORM_RECYCLING = 4
 
 // Rendering constants
 const PLATFORM_ALTITUDE = 300
 const PLATFORM_ANGULAR_WIDTH = 2 * Math.PI / TOTAL_PLATFORMS
-const PLATFORM_HEIGHT = 40
+const PLATFORM_HEIGHT = 30
 
 class DefensePl {
-    constructor() {
+    constructor(n) {
+        this.cannons = []
+        this.n = n
+
         this._changeJob(PLATFORM_MISSING)
     }
 
@@ -32,6 +34,7 @@ class DefensePl {
             case PLATFORM_BUILDING:
                 if (this.progress === PLATFORM_BUILD_TIME) {
                     this._changeJob(PLATFORM_READY)
+                    this.cannons[0]._changeJob(CANNON_READY)
                 }
                 else {
                     ++this.progress
@@ -40,9 +43,9 @@ class DefensePl {
         }
     }
 
-    render(con, n) {
-        const a0 = PLATFORM_ANGULAR_WIDTH * n - 0.5 * PLATFORM_ANGULAR_WIDTH
-        const a1 = PLATFORM_ANGULAR_WIDTH * n + 0.5 * PLATFORM_ANGULAR_WIDTH
+    render(con) {
+        const a0 = PLATFORM_ANGULAR_WIDTH * this.n - 0.44 * PLATFORM_ANGULAR_WIDTH
+        const a1 = PLATFORM_ANGULAR_WIDTH * this.n + 0.44 * PLATFORM_ANGULAR_WIDTH
         const r0 = PLATFORM_ALTITUDE
         const r1 = PLATFORM_ALTITUDE + PLATFORM_HEIGHT
 
@@ -65,7 +68,7 @@ function renderDefenses(defenses, con, t) {
         const pl = defenses[n]
 
         if (pl.job === PLATFORM_BUILDING || pl.job === PLATFORM_READY) {
-            pl.render(con, n)
+            pl.render(con)
         }
     }
 
