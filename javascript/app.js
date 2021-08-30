4b06ceb189b4b236100d850e45186c84f5def1fe
Mark Vasilkov <mvasilkov@gmail.com>
2021-08-30 23:00:10 +0300

    Cannons on platforms

--- javascript/app.js
+++ javascript/app.js
@@ -3,6 +3,8 @@
 function update() {
     updateDefenses(state.defenses)
 
+    updateCannons(state.cannons)
+
     brownianMotion(state.invaders)
 }
 
@@ -12,6 +14,8 @@ function render(t) {
     paintBraille(cons.a, canvases.b, 0, 0, state.planet.render(), state.planet.enc)
 
     renderDefenses(state.defenses, cons.a, t)
+
+    renderCannons(state.defenses, cons.a, t)
 
     renderInvaders(state.invaders, cons.a, t)
 }
--- javascript/cannon.js
+++ javascript/cannon.js
@@ -17,4 +17,47 @@ class Cannon {
         this.progress = 0
         this.lastProgress = 0
     }
+
+    render(con, x, y) {
+        con.rect(x - 5, y - 5, 10, 10)
+    }
+}
+
+function updateCannons(cannons) {
+}
+
+function renderCannons(defenses, con, t) {
+    con.beginPath()
+
+    for (let n = 0; n < TOTAL_PLATFORMS; ++n) {
+        const pl = defenses[n]
+
+        if (pl.job === PLATFORM_BUILDING || pl.job === PLATFORM_READY) {
+            // This is a copy of DefensePl#render
+            const a0 = PLATFORM_ANGULAR_WIDTH * pl.n - 0.44 * PLATFORM_ANGULAR_WIDTH
+            const a1 = PLATFORM_ANGULAR_WIDTH * pl.n + 0.44 * PLATFORM_ANGULAR_WIDTH
+            const rb = PLATFORM_ALTITUDE + 0.5 * PLATFORM_HEIGHT
+
+            const x0 = rb * Math.cos(a0)
+            const y0 = rb * Math.sin(a0)
+            const x1 = rb * Math.cos(a1)
+            const y1 = rb * Math.sin(a1)
+
+            for (let cn = 0; cn < pl.level; ++cn) {
+                const ct = (cn + 1) / (pl.level + 1)
+
+                pl.cannons[cn].render(con,
+                    lerp(x0, x1, ct),
+                    lerp(y0, y1, ct))
+            }
+        }
+    }
+
+    con.closePath()
+
+    con.lineWidth = 2
+    con.strokeStyle = '#f00'
+    con.stroke()
+
+    con.restore() // saved in renderDefenses
 }
