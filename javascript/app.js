8fc3546fcd1bf603b55e39f3701037fa5d7bdab5
Mark Vasilkov <mvasilkov@gmail.com>
2021-09-11 19:38:01 +0300

    Prevent right click menu, drag and drop

--- javascript/app.js
+++ javascript/app.js
@@ -60,6 +60,32 @@ function render(t) {
 }
 
 function run() {
+    // Prevent unwanted browser features
+    document.body.addEventListener('contextmenu', event => {
+        event.preventDefault()
+    })
+
+    const preventDoubleTapZoom = (delay) => {
+        let beforeLastTouchStart = 0
+        let lastTouchStart = 0
+        document.addEventListener('touchstart', () => {
+            beforeLastTouchStart = lastTouchStart
+            lastTouchStart = Date.now()
+        })
+        document.addEventListener('touchend', (event) => {
+            const touchEnd = Date.now()
+            if (touchEnd - beforeLastTouchStart < delay) {
+                event.preventDefault()
+            }
+            const target = event.target
+            if (target && target.click) {
+                target.click()
+            }
+        })
+    }
+    preventDoubleTapZoom(400)
+
+    // Initialization
     initActions()
 
     resetState()
