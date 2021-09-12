6ffcdbed17c11f1b23ea2495e469062cbcc0d4a1
Mark Vasilkov <mvasilkov@gmail.com>
2021-09-12 03:27:48 +0300

    Build script, but I'm on a Windows machine and can't test it lol

--- build.sh
+++ build.sh
@@ -3,17 +3,18 @@
 rm -rf build
 mkdir -p build
 
-# npx html-minifier --collapse-whitespace --remove-attribute-quotes \
-#     -o build/index.html index_build.html
+npx html-minifier --collapse-whitespace --remove-attribute-quotes \
+    -o build/index.html index_build.html
 
 npx cleancss -O1 -o build/app.css stylesheets/app.css
 
 npx terser --enclose --compress --mangle -o build/app.js \
-    javascript/natlibMainloop.js javascript/natlibVec2.js javascript/noise.js \
-    javascript/palette.js javascript/braille.js javascript/setup.js \
+    javascript/audio/ZzFX.js javascript/audio/reverbgen.js javascript/audio/audio.js \
+    javascript/audio/oborona.js javascript/natlibMainloop.js javascript/natlibVec2.js \
+    javascript/noise.js javascript/palette.js javascript/braille.js javascript/setup.js \
     javascript/background.js javascript/planet.js javascript/cannon.js \
     javascript/defense.js javascript/invader.js javascript/rocket.js \
-    javascript/state.js javascript/actions.js javascript/app.js
+    javascript/debris.js javascript/state.js javascript/actions.js javascript/app.js
 
 python3 -c "import json; from pathlib import Path; "\
 "obj = json.loads(Path('manifest.json').read_text(encoding='utf-8')); "\
